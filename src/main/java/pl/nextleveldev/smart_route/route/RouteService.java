b8418d5dfda0b26522203ac75379f84b5b0e6100
RobertoJavaDev <robertojavadev@gmail.com>
2025-02-15 07:42:48 +0100

    feat: Added empty RoutService class

---
+++ src/main/java/pl/nextleveldev/smart_route/route/RouteService.java
@@ -0,0 +1,12 @@
+package pl.nextleveldev.smart_route.route;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+class RouteService {
+    List<?> searchRoute(final String startLocation, final String destination) {
+        return null;
+    }
+}
