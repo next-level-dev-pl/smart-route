70528b6b71a5f63f69687ca329156e31129c74a0
jkuznik <janusz.kuznik89@gmail.com>
2025-02-20 17:07:37 +0100

    Add: method to get stop info, Fix: increase UmWarsawClient max data buffer size

--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.nextleveldev.smart_route.infrastructure.um.api.UmBusLineResponse;
+import pl.nextleveldev.smart_route.infrastructure.um.api.UmStopInfoResponse;
 import pl.nextleveldev.smart_route.infrastructure.um.api.UmTimetableResponse;
 
 @Slf4j
@@ -60,6 +61,29 @@ public class UmWarsawClient {
                     log.debug("response: {}", json);
                     try {
                         return objectMapper.readValue(json, UmBusLineResponse.class);
+                    } catch (JsonProcessingException e) {
+                        // TODO: prepare proper exceptions
+                        throw new RuntimeException(e);
+                    }
+                })
+                .block();
+    }
+
+    public UmStopInfoResponse getStopInfo() {
+        return umWarsawWebClient.get()
+                .uri(urlBuilder -> urlBuilder.scheme("https")
+                        .path(properties.store().resourcePath())
+                        .queryParam("id", properties.store().stopInfoId())
+                        .queryParam("apikey", properties.apiKey())
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(json -> {
+                    // TODO: add specific response mapping
+                    log.debug("response: {}", json);
+                    try {
+                        return objectMapper.readValue(json, UmStopInfoResponse.class);
                     } catch (JsonProcessingException e) {
                         // TODO: prepare proper exceptions
                         throw new RuntimeException(e);
