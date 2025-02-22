b4cbcc070b6a882ad21150203b912f438912a915
Cezary Sanecki <saneckicezary@gmail.com>
2025-02-22 21:19:59 +0100

    Removed api client tests and provide int test abstraction

--- src/test/groovy/pl/nextleveldev/smart_route/route/api/RouteControllerITSpec.groovy
+++ src/test/groovy/pl/nextleveldev/smart_route/route/api/RouteControllerITSpec.groovy
@@ -1,20 +1,11 @@
 package pl.nextleveldev.smart_route.route.api
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
-import spock.lang.Specification
+import pl.nextleveldev.smart_route.BaseIntegrationSpec
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class RouteControllerITSpec extends Specification {
-
-    @Autowired
-    MockMvc mockMvc
+class RouteControllerITSpec extends BaseIntegrationSpec {
 
     def "should load spring context"() {
         when:
@@ -26,4 +17,5 @@ class RouteControllerITSpec extends Specification {
         then:
         result.andExpect(status().isOk())
     }
+
 }
