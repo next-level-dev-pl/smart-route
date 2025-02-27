de4267f3246138ca7782e95923ce4e0867b4edb9
RobertoJavaDev <robertojavadev@gmail.com>
2025-02-27 08:47:57 +0100

    feat: Formatted code

--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
@@ -16,41 +16,53 @@ public class UmWarsawClient {
     private final UmWarsawProperties properties;
 
     public UmTimetableResponse getTimetableFor(String stopId, String stopNr, String line) {
-        return umWarsawClient.get()
-                .uri(urlBuilder -> urlBuilder.scheme("https")
-                        .path(properties.timetable().resourcePath())
-                        .queryParam("id", properties.timetable().timetableId())
-                        .queryParam("busstopId", stopId)
-                        .queryParam("busstopNr", stopNr)
-                        .queryParam("line", line)
-                        .queryParam("apikey", properties.apiKey())
-                        .build())
+        return umWarsawClient
+                .get()
+                .uri(
+                        urlBuilder ->
+                                urlBuilder
+                                        .scheme("https")
+                                        .path(properties.timetable().resourcePath())
+                                        .queryParam("id", properties.timetable().timetableId())
+                                        .queryParam("busstopId", stopId)
+                                        .queryParam("busstopNr", stopNr)
+                                        .queryParam("line", line)
+                                        .queryParam("apikey", properties.apiKey())
+                                        .build())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body(UmTimetableResponse.class);
     }
 
     public UmBusLineResponse getBusLineFor(String stopId, String stopNr) {
-        return umWarsawClient.get()
-                .uri(urlBuilder -> urlBuilder.scheme("https")
-                        .path(properties.timetable().resourcePath())
-                        .queryParam("id", properties.timetable().busLineId())
-                        .queryParam("busstopId", stopId)
-                        .queryParam("busstopNr", stopNr)
-                        .queryParam("apikey", properties.apiKey())
-                        .build())
+        return umWarsawClient
+                .get()
+                .uri(
+                        urlBuilder ->
+                                urlBuilder
+                                        .scheme("https")
+                                        .path(properties.timetable().resourcePath())
+                                        .queryParam("id", properties.timetable().busLineId())
+                                        .queryParam("busstopId", stopId)
+                                        .queryParam("busstopNr", stopNr)
+                                        .queryParam("apikey", properties.apiKey())
+                                        .build())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body(UmBusLineResponse.class);
     }
 
     public UmStopInfoResponse getStopInfo() {
-        return umWarsawClient.get()
-                .uri(urlBuilder -> urlBuilder.scheme("https")
-                        .path(properties.store().resourcePath())
-                        .queryParam("id", properties.store().stopInfoId())
-                        .queryParam("apikey", properties.apiKey())
-                        .build())
+        return umWarsawClient
+                .get()
+                .uri(
+                        urlBuilder ->
+                                urlBuilder
+                                        .scheme("https")
+                                        .path(properties.store().resourcePath())
+                                        .queryParam("id", properties.store().stopInfoId())
+                                        .queryParam("apikey", properties.apiKey())
+                                        .build())
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body(UmStopInfoResponse.class);
