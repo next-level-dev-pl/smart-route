ef6b864a234285d6b5eee65967bdb595f083d18b
jkuznik <janusz.kuznik89@gmail.com>
2025-03-10 15:24:01 +0100

    Timetable response mapper

--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawResponseMapper.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawResponseMapper.java
@@ -1,11 +1,19 @@
 package pl.nextleveldev.smart_route.infrastructure.um;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import pl.nextleveldev.smart_route.infrastructure.um.UmWarsawClient.UmWarsawBusStopGenericResponse;
 import pl.nextleveldev.smart_route.infrastructure.um.api.UmBusLineResponse;
 import pl.nextleveldev.smart_route.infrastructure.um.api.UmTimetableResponse;
 
 import static pl.nextleveldev.smart_route.infrastructure.um.UmWarsawClient.*;
+import static pl.nextleveldev.smart_route.infrastructure.um.api.UmTimetableResponse.*;
 
 class UmWarsawResponseMapper {
 
@@ -32,6 +40,57 @@ class UmWarsawResponseMapper {
     }
 
     static UmTimetableResponse mapTimetableResponse(String stopId, String stopNr, String line, UmWarsawTimetableGenericResponse genericResponse) {
+        List<Map<String, String>> parsedJson = new ArrayList<>();
+        genericResponse.result().stream()
+                .map(ResultValues::values)
+                .forEach(
+                        resultValues -> {
+                            Map<String, String> map = new HashMap<>();
+                            resultValues.forEach(
+                                    value -> {
+                                        map.put(value.key(), value.value());
+                                    });
+                            parsedJson.add(map);
+                        });
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        List<Result> results = new ArrayList<>();
+        parsedJson.forEach(
+                parsedJsonElement -> {
+                    String symbolOne =
+                            !parsedJsonElement.get("symbol_1").equals("null")
+                                    ? parsedJsonElement.get("symbol_1")
+                                    : null;
+
+                    String symbolTwo =
+                            !parsedJsonElement.get("symbol_2").equals("null")
+                                    ? parsedJsonElement.get("symbol_2")
+                                    : null;
+
+                    Integer brigade =
+                            !parsedJsonElement.get("brygada").equals("null")
+                                    ? Integer.parseInt(parsedJsonElement.get("brygada"))
+                                    : null;
+
+                    String direction =
+                            !parsedJsonElement.get("kierunek").equals("null")
+                                    ? parsedJsonElement.get("kierunek")
+                                    : null;
+
+                    String route =
+                            !parsedJsonElement.get("trasa").equals("null")
+                                    ? parsedJsonElement.get("trase")
+                                    : null;
+
+                    LocalTime time =
+                            !parsedJsonElement.get("czas").equals("null")
+                                    ? LocalTime.parse(
+                                    parsedJsonElement.get("czas"), formatter)
+                                    : null;
+                    results.add(new Result(symbolOne, symbolTwo, brigade, direction, route, time));
+                }
+        );
+
+        return new UmTimetableResponse(stopId, stopNr, line, results);
     }
 }
--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/api/UmTimetableResponse.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/api/UmTimetableResponse.java
@@ -1,7 +1,8 @@
 package pl.nextleveldev.smart_route.infrastructure.um.api;
 
 import java.time.LocalTime;
+import java.util.List;
 
-public record UmTimetableResponse(String stopId, String stopNr, String line, Result result){
-    record Result(String symbolOne, String symbolTwo, Integer brigade, String direction, String route, LocalTime time){}
+public record UmTimetableResponse(String stopId, String stopNr, String line, List<Result> result){
+    public record Result(String symbolOne, String symbolTwo, Integer brigade, String direction, String route, LocalTime time){}
 }
