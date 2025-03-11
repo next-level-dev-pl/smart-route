5b62bb217b6d202e0e26a81f67cebe16e69c5a2d
jkuznik <janusz.kuznik89@gmail.com>
2025-03-11 06:18:15 +0100

    Add custom exception

--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawClient.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 import pl.nextleveldev.smart_route.infrastructure.um.api.UmStopInfoResponse;
 
 @Slf4j
@@ -16,22 +17,34 @@ public class UmWarsawClient {
 
     public UmWarsawTimetableGenericResponse getTimetableFor(
             String stopId, String stopNr, String line) {
-        return umWarsawClient
-                .get()
-                .uri(
-                        urlBuilder ->
-                                urlBuilder
-                                        .scheme("https")
-                                        .path(properties.timetable().resourcePath())
-                                        .queryParam("id", properties.timetable().timetableId())
-                                        .queryParam("busstopId", stopId)
-                                        .queryParam("busstopNr", stopNr)
-                                        .queryParam("line", line)
-                                        .queryParam("apikey", properties.apiKey())
-                                        .build())
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .body(UmWarsawTimetableGenericResponse.class);
+        try {
+            return umWarsawClient
+                    .get()
+                    .uri(
+                            urlBuilder ->
+                                    urlBuilder
+                                            .scheme("https")
+                                            .path(properties.timetable().resourcePath())
+                                            .queryParam("id", properties.timetable().timetableId())
+                                            .queryParam("busstopId", stopId)
+                                            .queryParam("busstopNr", stopNr)
+                                            .queryParam("line", line)
+                                            .queryParam("apikey", properties.apiKey())
+                                            .build())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(UmWarsawTimetableGenericResponse.class);
+        } catch (RestClientException e) {
+            throw new TimetableResponseException(
+                    "Failed to receive response for timetable for stop Id:"
+                            + stopId
+                            + " and stop number: "
+                            + stopNr
+                            + " and line: "
+                            + line
+                            + ". Error: "
+                            + e.getMessage());
+        }
     }
 
     public UmWarsawBusStopGenericResponse getBusLineFor(String stopId, String stopNr) {
--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawResponseMapper.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawResponseMapper.java
@@ -42,62 +42,74 @@ class UmWarsawResponseMapper {
             String stopNr,
             String line,
             UmWarsawTimetableGenericResponse genericResponse) {
-        List<Map<String, String>> parsedJson = new ArrayList<>();
-        genericResponse
-                .result()
-                .forEach(
-                        resultValues -> {
-                            Map<String, String> map = new HashMap<>();
-                            resultValues.forEach(
-                                    value -> {
-                                        map.put(value.key(), value.value());
-                                    });
-                            parsedJson.add(map);
-                        });
+        try {
+            List<Map<String, String>> parsedJson = new ArrayList<>();
+            genericResponse
+                    .result()
+                    .forEach(
+                            resultValues -> {
+                                Map<String, String> map = new HashMap<>();
+                                resultValues.forEach(
+                                        value -> {
+                                            map.put(value.key(), value.value());
+                                        });
+                                parsedJson.add(map);
+                            });
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        List<Result> results = new ArrayList<>();
-        parsedJson.forEach(
-                parsedJsonElement -> {
-                    String symbolOne =
-                            parsedJsonElement.get("symbol_1") != null
-                                    ? parsedJsonElement.get("symbol_1")
-                                    : null;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            List<Result> results = new ArrayList<>();
+            parsedJson.forEach(
+                    parsedJsonElement -> {
+                        String symbolOne =
+                                parsedJsonElement.get("symbol_1") != null
+                                        ? parsedJsonElement.get("symbol_1")
+                                        : null;
 
-                    String symbolTwo =
-                            parsedJsonElement.get("symbol_2") != null
-                                    ? parsedJsonElement.get("symbol_2")
-                                    : null;
+                        String symbolTwo =
+                                parsedJsonElement.get("symbol_2") != null
+                                        ? parsedJsonElement.get("symbol_2")
+                                        : null;
 
-                    Integer brigade =
-                            parsedJsonElement.get("brygada") != null
-                                    ? Integer.parseInt(parsedJsonElement.get("brygada"))
-                                    : null;
+                        Integer brigade =
+                                parsedJsonElement.get("brygada") != null
+                                        ? Integer.parseInt(parsedJsonElement.get("brygada"))
+                                        : null;
 
-                    String direction =
-                            parsedJsonElement.get("kierunek") != null
-                                    ? parsedJsonElement.get("kierunek")
-                                    : null;
+                        String direction =
+                                parsedJsonElement.get("kierunek") != null
+                                        ? parsedJsonElement.get("kierunek")
+                                        : null;
 
-                    String route =
-                            parsedJsonElement.get("trasa") != null
-                                    ? parsedJsonElement.get("trasa")
-                                    : null;
+                        String route =
+                                parsedJsonElement.get("trasa") != null
+                                        ? parsedJsonElement.get("trasa")
+                                        : null;
 
-                    LocalTime time =
-                            parsedJsonElement.get("czas") != null
-                                    ? LocalTime.parse(
-                                            parsedJsonElement.get("czas").startsWith("24:")
-                                                    ? "00:"
-                                                            + parsedJsonElement
-                                                                    .get("czas")
-                                                                    .substring(3)
-                                                    : parsedJsonElement.get("czas"),
-                                            formatter)
-                                    : null;
-                    results.add(new Result(symbolOne, symbolTwo, brigade, direction, route, time));
-                });
+                        LocalTime time =
+                                parsedJsonElement.get("czas") != null
+                                        ? LocalTime.parse(
+                                        parsedJsonElement.get("czas").startsWith("24:")
+                                                ? "00:"
+                                                + parsedJsonElement
+                                                .get("czas")
+                                                .substring(3)
+                                                : parsedJsonElement.get("czas"),
+                                        formatter)
+                                        : null;
+                        results.add(new Result(symbolOne, symbolTwo, brigade, direction, route, time));
+                    });
 
-        return new UmTimetableResponse(stopId, stopNr, line, results);
+            return new UmTimetableResponse(stopId, stopNr, line, results);
+        } catch (Exception e) {
+            throw new TimetableResponseException(
+                    "Failed to receive response for timetable for stop Id:"
+                    + stopId
+                    + " and stop number: "
+                    + stopNr
+                    + " and line: "
+                    + line
+                    + ". Error: "
+                    + e.getMessage());
+        }
     }
 }
