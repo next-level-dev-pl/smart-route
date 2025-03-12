2b086ca4ac1cb4a31f673c582b9a0befb62b44ad
jkuznik <janusz.kuznik89@gmail.com>
2025-03-12 08:26:16 +0100

    Fix code

--- src/main/java/pl/nextleveldev/smart_route/SmartRouteApplication.java
+++ src/main/java/pl/nextleveldev/smart_route/SmartRouteApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public classlogSmartRouteApplication {
+public class SmartRouteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartRouteApplication.class, args);
