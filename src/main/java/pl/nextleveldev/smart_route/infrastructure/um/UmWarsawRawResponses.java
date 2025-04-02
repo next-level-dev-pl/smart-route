1f31c553348908689187a3f3e59d18ae9852f89d
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-02 16:54:02 +0200

    Made raw responses package private

--- src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawRawResponses.java
+++ src/main/java/pl/nextleveldev/smart_route/infrastructure/um/UmWarsawRawResponses.java
@@ -3,20 +3,20 @@ package pl.nextleveldev.smart_route.infrastructure.um;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-public class UmWarsawRawResponses {
+class UmWarsawRawResponses {
 
-    public record Timetable(List<List<Value>> result) {
+    record Timetable(List<List<Value>> result) {
         record Value(String key, String value) {}
     }
 
     record BusLine(@JsonProperty("result") List<ResultValue> results) {
-        public record ResultValue(List<ResultValue.Value> values) {
-            public record Value(String key, String value) {}
+        record ResultValue(List<ResultValue.Value> values) {
+            record Value(String key, String value) {}
         }
     }
 
-    public record StopInfo(@JsonProperty("result") List<ResultValue> results) {
-        public record ResultValue(List<ResultValue.Value> values) {
+    record StopInfo(@JsonProperty("result") List<ResultValue> results) {
+        record ResultValue(List<ResultValue.Value> values) {
             public record Value(String key, String value) {}
         }
     }
