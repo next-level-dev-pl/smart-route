afffe4dfe40381274b0b68e83a0004cfe0a37508
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-06 23:15:50 +0200

    Added parametrized cron

--- src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
+++ src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
@@ -1,6 +1,7 @@
 package pl.nextleveldev.smart_route.busstop;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import pl.nextleveldev.smart_route.infrastructure.um.UmWarsawClient;
 import pl.nextleveldev.smart_route.utils.Gatherers;
@@ -12,6 +13,7 @@ class BusStopImporter {
     private final UmWarsawClient umWarsawClient;
     private final BusStopRepository busStopRepository;
 
+    @Scheduled(cron = "${bus-stops.importer.cron}")
     public void importBusStops() {
         var response = umWarsawClient.getStopInfo();
 
