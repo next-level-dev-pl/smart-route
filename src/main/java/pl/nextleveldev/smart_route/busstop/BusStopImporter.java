153b3b35e561b0ca776a098d1bdf1a21546e8a58
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-08 10:18:43 +0200

    Fixed importer

--- src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
+++ src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
@@ -1,20 +1,28 @@
 package pl.nextleveldev.smart_route.busstop;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import pl.nextleveldev.smart_route.infrastructure.um.UmWarsawClient;
 import pl.nextleveldev.smart_route.utils.Gatherers;
 
+import java.util.Objects;
+import java.util.Optional;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
-class BusStopImporter {
+public class BusStopImporter {
 
     private final UmWarsawClient umWarsawClient;
     private final BusStopRepository busStopRepository;
 
+    @Transactional
     @Scheduled(cron = "${bus-stops.importer.cron}")
     public void importBusStops() {
+        log.info("Importing bus stops...");
         var response = umWarsawClient.getStopInfo();
 
         response.stream()
@@ -22,13 +30,15 @@ class BusStopImporter {
                         stopInfo.stopId(),
                         stopInfo.stopNr(),
                         stopInfo.stopIdName(),
-                        stopInfo.streetId().toString(),
+                        Optional.ofNullable(stopInfo.streetId()).map(Objects::toString).orElse(null),
                         stopInfo.location(),
                         stopInfo.direction(),
                         stopInfo.validFrom()
                 ))
                 .collect(Gatherers.groupingByFixedSize(10))
                 .forEach(busStopRepository::saveAll);
+
+        log.info("Bus stops imported.");
     }
 
 
