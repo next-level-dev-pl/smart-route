81901e90a3ac34586bfa2129eee9aa61c0c54f8a
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-08 10:19:49 +0200

    Made importer package private

--- src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
+++ src/main/java/pl/nextleveldev/smart_route/busstop/BusStopImporter.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class BusStopImporter {
+class BusStopImporter {
 
     private final UmWarsawClient umWarsawClient;
     private final BusStopRepository busStopRepository;
