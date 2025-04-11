c609dd28f1b6af120f39bbcefad8e386e04fe395
Cezary Sanecki <saneckicezary@gmail.com>
2025-04-11 21:39:37 +0200

    Removed entity graph

--- src/main/java/pl/nextleveldev/smart_route/busstop/BusStopRepository.java
+++ src/main/java/pl/nextleveldev/smart_route/busstop/BusStopRepository.java
@@ -1,14 +1,8 @@
 package pl.nextleveldev.smart_route.busstop;
 
-import java.util.Optional;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BusStopRepository extends JpaRepository<BusStop, Long> {
-
-    @EntityGraph(attributePaths = {"lines"})
-    Optional<BusStop> findById(UUID uuid);
 }
